e60d88747604962373365f6171e208543588ccf2
Cleyton Jesus <cleytonjesus007@gmail.com>
2022-12-30 14:51:51 -0300

    fix musics tiles #2

--- pageComponents/InicioPage/index.jsx
+++ pageComponents/InicioPage/index.jsx
@@ -20,8 +20,8 @@ function InicioPage({ songsYouMightLike, recents, data, setAlbum, setPage, getAr
                                         <div className="max-md:hidden absolute  overflow-hidden   bg-neutral-900  bg-center bg-no-repeat bg-cover" style={{ width: 90, height: 90, left: "-20px", borderRadius: "0 100px 100px 0" }}>
                                             <Image src={album_cover} decoding="sync" loading="eager" fill sizes="(max-width: 768px) 100%,(max-width: 1200px) 50%,33%" style={{ objectFit: "cover", objectPosition: "top" }} alt="album cover" />
                                         </div>
-                                        <div className="flex  bg-black bg-opacity-70  w-full px-10" style={{ justifyContent: "end", alignItems: "center" }}>
-                                            <div className=" text-sm font-bold  bg-black bg-opacity-70   rounded-md  p-2 text-center " style={{ zIndex: 10 }} ><span className="line-clamp-2">{music_title}</span></div>
+                                        <div className="flex justify-center items-center  bg-black bg-opacity-70 w-full px-3 ">
+                                            <div className=" text-sm font-bold w-full  bg-black bg-opacity-70   rounded-md  p-2 text-center z-50 "  ><span className="line-clamp-2">{music_title}</span></div>
                                         </div>
                                     </div>
                                 )
